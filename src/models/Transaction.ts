c994e961d23f22f97b5a242c6ea762a9c39ecebb
Pedro Amaral <pedrotaem@gmail.com>
2020-04-26 03:14:26 -0300

    challange finalized

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -17,14 +17,17 @@ class Transaction {
   @Column()
   title: string;
 
-  @Column()
-  type: 'income' | 'outcome';
-
   @Column('decimal')
   value: number;
 
+  @Column()
+  type: 'income' | 'outcome';
+
   @ManyToOne(() => Category)
   @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
   @CreateDateColumn()
